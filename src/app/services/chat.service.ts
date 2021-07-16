8207026c7eec94be6cce8c6f4657298d2a5011b6
MaryKattyVO <mkvicunaore@gmail.com>
2021-07-16 11:53:04 -0500

    detalles finales

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -29,9 +29,16 @@ export class ChatService {
   }
 
   login( proveedor: string) {
-    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+    if( proveedor === 'google') {
+      this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    } else {
+      this.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+    }
+    
   }
   logout() {
+    this.usuario = {};
     this.auth.signOut();
   }
 
@@ -54,9 +61,10 @@ export class ChatService {
 
   agregarMensaje( texto: string ) {
     let mensaje: Mensaje = {
-      nombre: 'Demo',
+      nombre: this.usuario.nombre,
       mensaje: texto,
-      fecha: new Date().getTime()
+      fecha: new Date().getTime(),
+      uid: this.usuario.uid
     }
 
     return this.itemsCollection.add(mensaje);
